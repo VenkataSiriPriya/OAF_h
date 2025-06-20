7da0044da56f5bf3a2f378cee38c7ce703afae06
Hemanth-Moka <2300030439@kluniversity.in>
2025-06-20 11:12:50 +0530

    Version 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./styles/navbar.css";
+import "./styles/Navbar.css";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
